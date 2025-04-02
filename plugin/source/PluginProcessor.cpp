b15f08bcae0b9b0066036893e825f4d308822ba3
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2025-04-02 11:18:53 +0200

    Apply tremolo to input

--- plugin/source/PluginProcessor.cpp
+++ plugin/source/PluginProcessor.cpp
@@ -1,5 +1,6 @@
 #include "Tremolo/PluginProcessor.h"
 #include <juce_core/juce_core.h>
+#include <juce_dsp/juce_dsp.h>
 #include "Tremolo/PluginEditor.h"
 
 namespace ws {
@@ -126,20 +127,12 @@ void AudioPluginAudioProcessor::processBlock(juce::AudioBuffer<float>& buffer,
   // This is here to avoid people getting screaming feedback
   // when they first compile a plugin, but obviously you don't need to keep
   // this code if your algorithm always overwrites all the output channels.
-  for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+  for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i) {
     buffer.clear(i, 0, buffer.getNumSamples());
-
-  // This is the place where you'd normally do the guts of your plugin's
-  // audio processing...
-  // Make sure to reset the state if your inner loop is processing
-  // the samples and the outer loop is handling the channels.
-  // Alternatively, you can process the samples with the channels
-  // interleaved by keeping the same state.
-  for (int channel = 0; channel < totalNumInputChannels; ++channel) {
-    auto* channelData = buffer.getWritePointer(channel);
-    juce::ignoreUnused(channelData);
-    // ..do something to the data...
   }
+
+  juce::dsp::AudioBlock<float> block{buffer};
+  tremolo.process(juce::dsp::ProcessContextReplacing<float>{block});
 }
 
 bool AudioPluginAudioProcessor::hasEditor() const {
