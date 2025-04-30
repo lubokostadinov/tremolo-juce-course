e6a1926051c743a9ed1e5aeb294acdbee57b10f7
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2025-04-30 13:36:07 +0200

    Added const

--- todo/tremolo_plugin/source/PluginProcessor.cpp
+++ todo/tremolo_plugin/source/PluginProcessor.cpp
@@ -92,8 +92,8 @@ void PluginProcessor::processBlock(juce::AudioBuffer<float>& buffer,
   juce::ignoreUnused(midiMessages);
 
   juce::ScopedNoDenormals noDenormals;
-  auto totalNumInputChannels = getTotalNumInputChannels();
-  auto totalNumOutputChannels = getTotalNumOutputChannels();
+  const auto totalNumInputChannels = getTotalNumInputChannels();
+  const auto totalNumOutputChannels = getTotalNumOutputChannels();
 
   // In case we have more outputs than inputs, this code clears any output
   // channels that didn't contain input data, (because these aren't
