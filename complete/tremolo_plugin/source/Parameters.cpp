958fc393265ce6892a274249331de2a402aeb23c
reuk <reuk@users.noreply.github.com>
2025-06-23 13:14:57 +0100

    Avoid intermediate Container of parameters

--- complete/tremolo_plugin/source/Parameters.cpp
+++ complete/tremolo_plugin/source/Parameters.cpp
@@ -1,39 +1,42 @@
 
 namespace tremolo {
 namespace {
+template <typename Param>
+Param& addParamToProcessor(juce::AudioProcessor& processor,
+                           std::unique_ptr<Param> param) {
+  auto& result = *param;
+  processor.addParameter(param.release());
+  return result;
+}
+
 juce::AudioParameterFloat& createModulationRateParameter(
-    Parameters::Container& c) {
+    juce::AudioProcessor& c) {
   constexpr auto versionHint = 1;
-  auto parameter = std::make_unique<juce::AudioParameterFloat>(
-      juce::ParameterID{"modulation.rate", versionHint}, "Modulation rate",
-      juce::NormalisableRange<float>{0.1f, 20.f, 0.01f, 0.4f}, 5.f,
-      juce::AudioParameterFloatAttributes{}.withLabel("Hz"));
-  auto& parameterReference = *parameter;
-  c.push_back(std::move(parameter));
-  return parameterReference;
+  return addParamToProcessor(
+      c,
+      std::make_unique<juce::AudioParameterFloat>(
+          juce::ParameterID{"modulation.rate", versionHint}, "Modulation rate",
+          juce::NormalisableRange<float>{0.1f, 20.f, 0.01f, 0.4f}, 5.f,
+          juce::AudioParameterFloatAttributes{}.withLabel("Hz")));
 }
 
-juce::AudioParameterBool& createBypassedParameter(Parameters::Container& c) {
+juce::AudioParameterBool& createBypassedParameter(juce::AudioProcessor& c) {
   constexpr auto versionHint = 1;
-  auto parameter = std::make_unique<juce::AudioParameterBool>(
-      juce::ParameterID{"bypassed", versionHint}, "Bypass", false);
-  auto& parameterReference = *parameter;
-  c.push_back(std::move(parameter));
-  return parameterReference;
+  return addParamToProcessor(
+      c, std::make_unique<juce::AudioParameterBool>(
+             juce::ParameterID{"bypassed", versionHint}, "Bypass", false));
 }
 
-juce::AudioParameterChoice& createWaveformParameter(Parameters::Container& c) {
+juce::AudioParameterChoice& createWaveformParameter(juce::AudioProcessor& c) {
   constexpr auto versionHint = 1;
-  auto parameter = std::make_unique<juce::AudioParameterChoice>(
-      juce::ParameterID{"modulation.waveform", versionHint},
-      "Modulation waveform", juce::StringArray{"Sine", "Triangle"}, 0);
-  auto& parameterReference = *parameter;
-  c.push_back(std::move(parameter));
-  return parameterReference;
+  return addParamToProcessor(
+      c, std::make_unique<juce::AudioParameterChoice>(
+             juce::ParameterID{"modulation.waveform", versionHint},
+             "Modulation waveform", juce::StringArray{"Sine", "Triangle"}, 0));
 }
 }  // namespace
 
-Parameters::Parameters(Container& parameterContainer)
+Parameters::Parameters(juce::AudioProcessor& parameterContainer)
     : rate{createModulationRateParameter(parameterContainer)},
       bypassed{createBypassedParameter(parameterContainer)},
       waveform{createWaveformParameter(parameterContainer)} {}
