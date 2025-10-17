b0ca3cb1396d3de99fab6b6580f0444a5594e7cd
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2025-10-17 13:18:06 +0200

    Display "message" on hover

--- complete/tremolo_plugin/include/Tremolo/AboutComponent.h
+++ complete/tremolo_plugin/include/Tremolo/AboutComponent.h
@@ -3,7 +3,11 @@
 namespace tremolo {
 class AboutComponent : public juce::Component {
 public:
-  AboutComponent() = default;
+  AboutComponent() {
+    // popup.setAllowedPlacement(juce::BubbleComponent::BubblePlacement::below);
+    // getParentComponent()->addAndMakeVisible(popup);
+    // addChildComponent(popup);
+  }
 
   void paint(juce::Graphics& g) override {
     g.setColour(juce::Colour{0xFF315160});
@@ -12,5 +16,25 @@ public:
     g.setFont(CustomLookAndFeel::getSideLabelsFont());
     g.drawFittedText("i", getLocalBounds(), juce::Justification::centred, 1);
   }
+
+  void mouseEnter(const juce::MouseEvent& event) override {
+    popup.reset(new juce::BubbleMessageComponent);
+    popup->setAllowedPlacement(juce::BubbleComponent::BubblePlacement::below);
+    popup->setAlwaysOnTop(true);
+    popup->addToDesktop(0);
+
+    juce::AttributedString text{"message"};
+    text.setColour(juce::Colours::orange);
+    text.setJustification(juce::Justification::centred);
+
+    popup->showAt(this, text, 0, false);
+  }
+
+  void mouseExit(const juce::MouseEvent& event) override {
+    popup->setVisible(false);
+  }
+
+private:
+  std::unique_ptr<juce::BubbleMessageComponent> popup;
 };
 }  // namespace tremolo
