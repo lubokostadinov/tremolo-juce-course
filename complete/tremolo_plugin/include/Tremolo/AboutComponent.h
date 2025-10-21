4d3ded82cc4dd2383e53a5223cb290bf9b3e1f48
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2025-10-21 12:59:34 +0200

    Removed addToDesktop()

--- complete/tremolo_plugin/include/Tremolo/AboutComponent.h
+++ complete/tremolo_plugin/include/Tremolo/AboutComponent.h
@@ -7,10 +7,6 @@ public:
     popup.setAllowedPlacement(juce::BubbleComponent::BubblePlacement::below);
     popup.setAlwaysOnTop(true);
 
-    // since the popup is displayed outside the editor window,
-    // we have to add it to the desktop
-    popup.addToDesktop(0);
-
     text.setColour(
         CustomLookAndFeel::getColor(CustomLookAndFeel::Colors::paleBlue));
     text.setJustification(juce::Justification::centred);
@@ -21,6 +17,14 @@ public:
   ~About() override { target.removeMouseListener(this); }
 
   void mouseDown(const juce::MouseEvent&) override {
+    if (popup.getParentComponent() == nullptr) {
+      if (auto* parent = target.getParentComponent()) {
+        parent->addChildComponent(popup);
+      } else {
+        target.addChildComponent(popup);
+      }
+    }
+
     if (!popup.isVisible()) {
       popup.showAt(&target, text, 0, true);
     }
@@ -31,7 +35,7 @@ private:
   juce::BubbleMessageComponent popup;
   juce::AttributedString text{JucePlugin_Manufacturer
                               "\n" JucePlugin_Name "\n" __DATE__ " " __TIME__
-                              " v" JucePlugin_VersionString};
+                              "\nv" JucePlugin_VersionString};
 
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(About)
 };
